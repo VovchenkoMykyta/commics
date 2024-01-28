20df095b6a06b7670a3547a7303dcf9e0250717b
maks <maksjob01@gmail.com>
2024-01-28 22:43:31 +0200

    Test pages

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,5 @@
+@extends('welcome')
+
+@section('main')
+    <div>Home page</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\HomeController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/', [HomeController::class, 'index'])->name('home-page');
+
+Route::get('/test', function () {
     return view('welcome');
 });
